2f89ea709d76d1fcc646432cb28edf8d140efd8e
Granah <infodiegohermoso@gmail.com>
2022-02-27 19:35:11 +0100

    feat add product not found test

--- tests/src/CartShop/Product/Application/Delete/DeleteProductCommandHandlerTest.php
+++ tests/src/CartShop/Product/Application/Delete/DeleteProductCommandHandlerTest.php
@@ -6,6 +6,7 @@ namespace Granah\CartShop\Tests\CartShop\Product\Application\Delete;
 use Granah\CartShop\Product\Application\Delete\DeleteProduct;
 use Granah\CartShop\Product\Application\Delete\DeleteProductCommandHandler;
 use Granah\CartShop\Product\Domain\ProductId;
+use Granah\CartShop\Product\Domain\ProductNotFound;
 use Granah\CartShop\Tests\CartShop\Product\Domain\ProductMother;
 use Granah\CartShop\Tests\CartShop\Product\ProductModuleUnitTestCase;
 
@@ -27,6 +28,19 @@ final class DeleteProductCommandHandlerTest extends ProductModuleUnitTestCase
         $product = ProductMother::create(new ProductId($command->id()));
         $this->shouldSearch($product);
         $this->shouldDelete();
+
+        $this->dispatch($command, $this->handler);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_throw_exception_when_product_not_found(): void
+    {
+        $command = DeleteProductCommandMother::create();
+        $this->shouldSearch(null);
+
+        $this->expectException(ProductNotFound::class);
 
         $this->dispatch($command, $this->handler);
     }
